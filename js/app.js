d54e5bdb871969a51813ead82eaf64c8e4d9b053
Cherry Liu <cherryliu1227@gmail.com>
2024-06-19 08:13:57 -0400

    project

---
+++ js/app.js
@@ -0,0 +1,345 @@
+var videoLinks = {
+  A: [
+    {
+      song: "All Star - Smash Mouth",
+      link: "https://www.youtube.com/embed/em9lziI07M4?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=em9lziI07M4",
+    },
+    {
+      song: "Astronomia (Coffin Dance) - Vicetone & Tony Igy",
+      link: "https://www.youtube.com/embed/wTkLLQjgoEU?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=10&rel=0&playlist=wTkLLQjgoEU",
+    },
+    {
+      song: "Axel F - Crazy Frog",
+      link: "https://www.youtube.com/embed/k85mRPqvMbE?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=5&rel=0&playlist=k85mRPqvMbE",
+    },
+  ],
+  B: [
+    {
+      song: "Blue (Da Ba Dee) - Eiffel 65",
+      link: "https://www.youtube.com/embed/68ugkg9RePc?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=33&rel=0&playlist=68ugkg9RePc",
+    },
+    {
+      song: "Bitch Lasagna - Pewdiepie",
+      link: "https://www.youtube.com/embed/6Dh-RL__uN4?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=6Dh-RL__uN4",
+    },
+    {
+      song: "Best Cry Ever",
+      link: "https://www.youtube.com/embed/ee925OTFBCA?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=ee925OTFBCA",
+    },
+  ],
+  C: [
+    {
+      song: "Curb Your Ethusiasm Theme (Frolic) - Luciano Michelini",
+      link: "https://www.youtube.com/embed/q9RAZxNdCk8?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=q9RAZxNdCk8",
+    },
+  ],
+  D: [
+    {
+      song: "Dragostea Din Tei - O-Zone",
+      link: "https://www.youtube.com/embed/YnopHCL1Jk8?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=52&rel=0&playlist=YnopHCL1Jk8",
+    },
+    {
+      song: "Dramatic Chipmunk",
+      link: "https://www.youtube.com/embed/y8Kyi0WNg40?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=y8Kyi0WNg40",
+    },
+    {
+      song: "DAMN DANIEL",
+      link: "https://www.youtube.com/embed/X3oDvz4YauE?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=X3oDvz4YauE",
+    },
+  ],
+  E: [
+    {
+      song: "Elevator (Trap Remix)",
+      link: "https://www.youtube.com/embed/eIp1HXk6twk?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=1&rel=0&playlist=eIp1HXk6twk",
+    },
+    {
+      song: "Emotional Damage - Steven He",
+      link: "https://www.youtube.com/embed/bHb5CFGYz1A?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=bHb5CFGYz1A",
+    },
+  ],
+  F: [
+    {
+      song: "Friday - Rebecca Black",
+      link: "https://www.youtube.com/embed/kfVsfOSbJY0?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=kfVsfOSbJY0",
+    },
+    {
+      song: "The Fox - Ylvis",
+      link: "https://www.youtube.com/embed/jofNR_WkoCE?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=35&rel=0&playlist=jofNR_WkoCE",
+    },
+  ],
+  G: [
+    {
+      song: "Gangnam Style - Psy",
+      link: "https://www.youtube.com/embed/9bZkp7q19f0?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=5&rel=0&playlist=9bZkp7q19f0",
+    },
+  ],
+  H: [
+    {
+      song: "HEYYEYAAEYAAAEYAEYAA - He man",
+      link: "https://www.youtube.com/embed/mBw3qzf4s18?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=mBw3qzf4s18",
+    },
+    {
+      song: "Harlem Shake - Baauer",
+      link: "https://www.youtube.com/embed/lh3FE61mrHU?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=lh3FE61mrHU",
+    },
+    {
+      song: "Have You Ever Had A Dream",
+      link: "https://www.youtube.com/embed/G7RgN9ijwE4?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=G7RgN9ijwE4",
+    },
+    {
+      song: "Huh Cat",
+      link: "https://www.youtube.com/embed/xVWeRnStdSA?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=xVWeRnStdSA",
+    },
+    {
+      song: "Hardest Name In Africa",
+      link: "https://www.youtube.com/embed/W85F-UmnbF4?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=33&rel=0&playlist=W85F-UmnbF4",
+    },
+  ],
+  I: [
+    {
+      song: "I am very glad, because I'm finally returning back home (Trololo) - Eduard Khil",
+      link: "https://www.youtube.com/embed/4TnAKurylA8?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=25&rel=0&playlist=4TnAKurylA8",
+    },
+  ],
+  J: [
+    {
+      song: "Just Do It - Shia LaBeouf",
+      link: "https://www.youtube.com/embed/ZXsQAXx_ao0?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=2&rel=0&playlist=ZXsQAXx_ao0",
+    },
+  ],
+  K: [
+    {
+      song: "Kung Fu Fighting",
+      link: "https://www.youtube.com/embed/bmfudW7rbG0?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=14&rel=0&playlist=bmfudW7rbG0",
+    },
+  ],
+  L: [
+    {
+      song: "Levan Polkka - Hatsune Miku",
+      link: "https://www.youtube.com/embed/tlXSMrGWK9M?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=tlXSMrGWK9M",
+    },
+    {
+      song: "Lion",
+      link: "https://www.youtube.com/embed/399syDv0bBM?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=399syDv0bBM",
+    },
+  ],
+  M: [
+    {
+      song: "My Heart Will Go On (Recorder)- Matt Mulholland",
+      link: "https://www.youtube.com/embed/X2WH8mHJnhM?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=17&rel=0&playlist=X2WH8mHJnhM",
+    },
+    {
+      song: "Mysterious Ticking Noise - Potter Puppet Pals",
+      link: "https://www.youtube.com/embed/Tx1XIm6q4r4?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=30&rel=0&playlist=Tx1XIm6q4r4",
+    },
+  ],
+  N: [
+    {
+      song: "Nyan Cat",
+      link: "https://www.youtube.com/embed/2yJgwwDcgV8?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=1&rel=0&playlist=2yJgwwDcgV8",
+    },
+    {
+      song: "Never Gonna Give You Up - Rick Astley",
+      link: "https://www.youtube.com/embed/GBIIQ0kP15E?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=GBIIQ0kP15E",
+    },
+  ],
+  O: [
+    {
+      song: "OOOOHHHHH (Rap Battle Parody) - Super Hot Fire",
+      link: "https://www.youtube.com/embed/i9dTnGKAJtQ?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=2&rel=0&playlist=i9dTnGKAJtQ",
+    },
+  ],
+  P: [
+    {
+      song: "PPAP - PIKOTARO",
+      link: "https://www.youtube.com/embed/5ZYn25NNtMs?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=6&rel=0&playlist=5ZYn25NNtMs",
+    },
+    {
+      song: "Pokemon Theme Song",
+      link: "https://www.youtube.com/embed/JuYeHPFR3f0?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=6&rel=0&playlist=JuYeHPFR3f0",
+    },
+  ],
+  Q: [
+    {
+      song: "Quagmire Song - Family Guy",
+      link: "https://www.youtube.com/embed/V7TC7M2G1aY?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=V7TC7M2G1aY",
+    },
+  ],
+  R: [
+    {
+      song: "Renai Circulation",
+      link: "https://www.youtube.com/embed/uKxyLmbOc0Q?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=uKxyLmbOc0Q",
+    },
+    {
+      song: "Ridin - Chamillionaire",
+      link: "https://www.youtube.com/embed/u-AmHSX6bVE?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=u-AmHSX6bVE",
+    },
+  ],
+  S: [
+    {
+      song: "Spongebob Squarepants Theme Song",
+      link: "https://www.youtube.com/embed/r9L4AseD-aA?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=r9L4AseD-aA",
+    },
+  ],
+  T: [
+    {
+      song: "Tunak Tunak Tun - Daler Mehndi",
+      link: "https://www.youtube.com/embed/92ydUdqWE1g?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=25&rel=0&playlist=92ydUdqWE1g",
+    },
+  ],
+  U: [
+    {
+      song: "Ultimate - Denzel Curry (Roll Safe Meme)",
+      link: "https://www.youtube.com/embed/8gKbraVbGyQ?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=8gKbraVbGyQ",
+    },
+  ],
+  V: [
+    {
+      song: "Vibing Cat",
+      link: "https://www.youtube.com/embed/mI4Wu0Hx3Vc?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=mI4Wu0Hx3Vc",
+    },
+  ],
+  W: [
+    {
+      song: "Wii Theme",
+      link: "https://www.youtube.com/embed/po-0n1BKW2w?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=po-0n1BKW2w",
+    },
+    {
+      song: "Windows XP Start Up",
+      link: "https://www.youtube.com/embed/7nQ2oiVqKHw?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=7nQ2oiVqKHw",
+    },
+  ],
+  X: [
+    {
+      song: "X-Files Theme",
+      link: "https://www.youtube.com/embed/v4-GcS1UQyg?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=v4-GcS1UQyg",
+    },
+  ],
+  Y: [
+    {
+      song: "Yakety Sax - Boots Randolph",
+      link: "https://www.youtube.com/embed/Zcq_xLi2NGo?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=3&rel=0&playlist=Zcq_xLi2NGo",
+    },
+    {
+      song: "Yee",
+      link: "https://www.youtube.com/embed/q6EoRBvdVPQ?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=3&rel=0&playlist=q6EoRBvdVPQ",
+    },
+  ],
+  Z: [
+    {
+      song: "zankoku na tenshi no thesis (Neon Genesis Evangelion Theme)",
+      link: "https://www.youtube.com/embed/1mrGt1Xzkwg?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=1mrGt1Xzkwg",
+    },
+  ],
+};
+
+// 1. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement("script");
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 2. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("youtube-video", {
+    events: {
+      onReady: onPlayerReady,
+      // onStateChange: onPlayerStateChange,
+    },
+  });
+}
+
+// 3. The API will call this function when the video player is ready.
+//      Once video player is ready, the video will be unmuted
+function onPlayerReady(event) {
+  setTimeout(() => {
+    player.unMute();
+  }, 100);
+}
+
+var youtubeId = "1mrGt1Xzkwg";
+var srcLink = `https://www.youtube.com/embed/${youtubeId}?enablejsapi=1&autoplay=1&rel=0&mute=1&controls=0&loop=1&start=0&rel=0&playlist=${youtubeId}`;
+console.log(srcLink);
+var iframe = document.getElementById("youtube-video");
+console.log(iframe.src);
+
+//event listener for A-Z buttons
+var letterBtns = document.querySelectorAll(".alphabet");
+var song = document.querySelector(".song");
+var initialScreen = document.querySelector(".windowxp");
+letterBtns.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    //reset color buttons
+    resetColor();
+    // Highlight the clicked button
+    btn.classList.add("selectedKey");
+    letter = btn.dataset.id;
+    changeSong(letter);
+  });
+});
+
+//function to reset color text in buttons
+function resetColor() {
+  letterBtns.forEach(function (btn) {
+    btn.classList.remove("selectedKey");
+  });
+}
+
+//function to play corresponding video
+function changeSong(letter) {
+  position = getLink(letter);
+  iframe.src = videoLinks[letter][position].link;
+  song.innerText = videoLinks[letter][position].song;
+  initialScreen.style.display = "none";
+  muteBtn.src = "./img/icon-unmute.png";
+  pauseBtn.src = "./img/icon-pause.png";
+}
+//function to get the src link from videoLinks array
+function getLink(letter) {
+  if (videoLinks[letter].length > 1) {
+    var position = randNum(videoLinks[letter].length);
+    return position;
+  } else return 0;
+}
+
+//function to generate random number based on number of video links in each alphabet
+function randNum(number) {
+  return Math.floor(Math.random() * number);
+}
+
+//button option to mute
+var muteBtn = document.getElementById("mute");
+muteBtn.addEventListener("click", function () {
+  if (player.isMuted()) {
+    player.unMute();
+    muteBtn.src = "./img/icon-unmute.png";
+  } else {
+    player.mute();
+    muteBtn.src = "./img/icon-mute.png";
+  }
+});
+
+//button to play pause
+var pauseBtn = document.getElementById("pause");
+pauseBtn.addEventListener("click", function () {
+  if (player.getPlayerState() == 1) {
+    //Returns true if video is playing
+    player.pauseVideo();
+    pauseBtn.src = "./img/icon-play.png";
+  } else if (player.getPlayerState() == 2) {
+    player.playVideo();
+    pauseBtn.src = "./img/icon-pause.png";
+  }
+});
+
+// button to shuffle
+var shuffleBtn = document.getElementById("shuffle");
+shuffleBtn.addEventListener("click", function () {
+  var letter = String.fromCharCode(65 + randNum(24));
+  resetColor();
+  changeSong(letter);
+  document.querySelector(`[data-id="${letter}"]`).classList.add("selectedKey");
+});
